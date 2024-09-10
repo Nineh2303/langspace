8b1af9c470dd18ef4ba205fa4436db4fbecb44c8
chinhnguyen <chinhnguyenduc2000@gmail.com>
2024-09-10 14:41:57 +0700

    change

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="fixed top-0 w-full flex flex-col items-center">
-    <div class="relative w-[80%] h-[100px] flex flex-row justify-between items-center max-[1400px]:justify-start">
+    <div class="relative w-[80%] h-[100px] flex flex-row justify-between items-center max-[1400px]:justify-between">
       <img src="@/assets/logo.jpg" class="h-[100px]" alt="">
       <ul class="flex flex-row items-center space-x-[100px] h-full bg-white transition-all duration-500 overflow-hidden
       max-2xl:space-x-[30px]
--- src/components/HeaderMenuComponent.vue
+++ src/components/HeaderMenuComponent.vue
@@ -1,13 +1,17 @@
 <template>
-  <div @mouseenter="isHover= true" @mouseleave="isHover= false" class="relative">
+  <div @mouseenter="isHover= true" @mouseleave="isHover= false" class="relative" @click="isHover =!isHover">
     <router-link to="/" class="flex items-center">{{ props.menuName }}
         <ChevronDownIcon v-if="isDropdown" class="size-8 pl-2"/>
     </router-link>
-    <div v-if="!isDropdown" class="bg-blue-600 h-1 transition-all  duration-300 max-lg:hidden" :class="[isHover ? 'w-full' : 'w-0']"/>
+    <div v-if="!isDropdown" class="bg-blue-600 h-1 transition-all  duration-500 max-lg:hidden" :class="[isHover ? 'w-full' : 'w-0']"/>
     <ul v-if="isDropdown"
-        :class="isHover ?'max-h-[400px]':'max-h-0'"
-        class="fixed bg-white mt-2 transition-all duration-500 w-[200px] z-10 overflow-hidden shadow-sm shadow-gray-500 rounded-lg">
-      <li v-for="(course,index) in dropDownData" class="h-[50px] w-full flex px-5 py-2">
+          :class="isHover ?'max-h-[400px]':'max-h-0'"
+        class="fixed bg-white h-[200px] mt-2 transition-all duration-500 w-[200px] z-10 overflow-hidden shadow-sm shadow-gray-500 rounded-lg
+        max-lg:w-[300px]
+">
+
+
+      <li v-for="(course,index) in dropDownData" class="h-[50px] w-full flex px-5 py-2 max-lg:">
         <router-link to="/"
                      class=" w-full"
                      :class="index+1==dropDownData?.length ? '' :'border-b-2'"
