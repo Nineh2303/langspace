a4ffbb3431bf81c0a692e3ba225a023f547a8f00
chinhnguyen <chinhnguyenduc2000@gmail.com>
2024-09-11 10:17:19 +0700

    test Mobile UI

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,28 +5,28 @@
       <ul class="flex flex-row items-center space-x-[100px] h-full bg-white transition-all duration-500 overflow-hidden
       max-2xl:space-x-[30px]
       max-lg:absolute lg-max:top-0 max-lg:flex-col max-lg:content-center
-      max-lg:items-start max-lg:w-full max-lg:top-[100px] max-lg:space-x-0"
-          :class="dropMenu ?' max-lg:h-[250px]' : 'max-lg:h-0'">
+      max-lg:items-start max-lg:w-full max-lg:top-[100px] max-lg:space-x-0 max-lg:h-auto"
+          :class="!dropMenu ?' max-lg:left-[-1000px]' : 'max-lg:left-0'">
         <!--        class=" w-full text-lg font-bold p-[5px] hover:bg-[#1030bf] hover:text-white rounded-[5px]"-->
-        <li class="flex items-center font-bold text-lg max-lg:border-b-2 max-lg:w-[95%] max-lg:h-[50px] px-5">
+        <li class="flex items-center font-bold text-lg max-lg:border-b-2 max-lg:w-[90%] px-5">
           <HeaderMenuComponent menu-name="Trang chủ" :is-dropdown="false"/>
         </li>
-        <li class="flex items-center font-bold text-lg max-lg:border-b-2 max-lg:w-[95%] max-lg:h-[50px] px-5">
+        <li class="flex items-center font-bold text-lg max-lg:border-b-2 max-lg:w-[90%] px-5">
           <HeaderMenuComponent menu-name="Langspace là ai" :is-dropdown="false"/>
         </li>
-        <li class="flex items-center font-bold text-lg max-lg:border-b-2 max-lg:w-[95%] max-lg:h-[50px] px-5">
+        <li class="flex items-center font-bold text-lg max-lg:border-b-2 max-lg:w-[90%] px-5">
           <HeaderMenuComponent menu-name="Các khóa học" :is-dropdown="true" :drop-down-data="listCourse"/>
         </li>
-        <li class="flex items-center font-bold text-lg max-lg:border-b-2 max-lg:w-[95%] max-lg:h-[50px] px-5">
+        <li class="flex items-center font-bold text-lg max-lg:border-b-2 max-lg:w-[90%] px-5">
           <HeaderMenuComponent menu-name="Lịch khai giảng" :is-dropdown="false"/>
         </li>
-        <li class="flex items-center font-bold text-lg max-lg:border-b-2 max-lg:w-[95%] max-lg:h-[50px] px-5">
+        <li class="flex items-center font-bold text-lg max-lg:border-b-2 max-lg:w-[90%] px-5">
           <HeaderMenuComponent menu-name="Block" :is-dropdown="false"/>
         </li>
-        <li class="flex items-center font-bold text-lg max-lg:w-full max-lg:h-[50px] lg:hidden">
+        <li class="flex items-center justify-center font-bold text-lg max-lg:w-full  lg:hidden">
           <button
               class="bg-[#1030bf] w-[200px] h-[35px] text-md text-white font-bold flex items-center justify-center rounded-lg hover:bg-[#ffbb01] hover:text-black
-              max-lg:w-full max-lg:mt-[10px] max-[1400px]:w-[100px]
+              max-lg:w-[400px] max-lg:mt-[25px] max-[1400px]:w-[100px]
 ">
             Đăng ký ngay
             <ArrowLongRightIcon class="size-6 ml-5 cursor-pointer"/>
--- src/components/HeaderMenuComponent.vue
+++ src/components/HeaderMenuComponent.vue
@@ -1,26 +1,30 @@
 <template>
-  <div @mouseenter="isHover= true" @mouseleave="isHover= false" class="relative" @click="isHover =!isHover">
-    <router-link to="/" class="flex items-center">{{ props.menuName }}
-        <ChevronDownIcon v-if="isDropdown" class="size-8 pl-2"/>
+  <div @mouseenter="isHover= true" @mouseleave="isHover= false" class="h-auto" @click="isHover =!isHover">
+    <router-link v-if="!isDropdown" to="/" class="flex flex-row items-center max-lg:h-[50px]">{{ props.menuName }}
     </router-link>
-    <div v-if="!isDropdown" class="bg-blue-600 h-1 transition-all duration-500 max-lg:hidden max-lg:w-[95%]" :class="[isHover ? 'w-full' : 'w-0']"/>
-    <ul v-if="isDropdown"
-          :class="isHover ?'max-h-[400px]':'max-h-0'"
-        class="fixed bg-white h-[200px] mt-2 transition-all duration-500 w-[200px] z-10 overflow-hidden shadow-sm shadow-gray-500 rounded-lg
-        max-lg:w-[300px]
-">
-
-
-      <li v-for="(course,index) in dropDownData" class="h-[50px] w-full flex px-5 py-2 max-lg:">
-        <router-link to="/"
-                     class=" w-[80%]"
-                     :class="index+1==dropDownData?.length ? '' :'border-b-2'"
-        >
-          {{course}}
+    <div  v-else class="flex flex-col overflow-hidden cursor-pointer max-lg:w-full">
+      <h1 class="flex flex-row items-center max-lg:h-[50px]">
+        {{ props.menuName }}
+        <ChevronDownIcon class="size-6"/>
+      </h1>
+      <div class="absolute px-5 top-[75px] overflow-hidden flex flex-col
+                  bg-white transition-all duration-500 rounded-lg shadow-sm shadow-gray-500
+                  max-lg:rounded-none max-lg:relative max-lg:ml-[10px] max-lg:top-0 max-lg:shadow-none"
+           :class="isHover ? 'max-h-[500px]' :'max-h-0'" >
+<!--           -->
+        <router-link to="/" v-for="(course, index) in dropDownData"
+                     class=" text-base py-3 max-lg:ml-[10px] max-lg:py-1 max-lg:border-none w-[175px] hover:text-white"
+             :class="index+1 == dropDownData?.length ? '' : 'border-b-2'">
+          {{ course }}
         </router-link>
-      </li>
-    </ul>
+
+      </div>
+    </div>
+    <div v-if="!isDropdown" class="bg-blue-600 h-1 transition-all duration-500 max-lg:hidden max-lg:w-[95%]"
+         :class="[isHover ? 'w-full' : 'w-0']"/>
+
   </div>
+
 </template>
 
 <script setup lang="ts">
@@ -30,7 +34,7 @@ import {ChevronDownIcon} from "@heroicons/vue/20/solid"
 interface HeaderMenuProps {
   menuName: string,
   isDropdown: boolean
-  dropDownData? :string  []
+  dropDownData?: string  []
 }
 
 const props = defineProps<HeaderMenuProps>()
