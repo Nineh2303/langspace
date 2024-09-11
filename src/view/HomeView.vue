944ddd4b525a2e794482809040f0546a4252c1ff
chinhnguyen <chinhnguyenduc2000@gmail.com>
2024-09-11 15:56:37 +0700

    test Mobile UI

--- src/view/HomeView.vue
+++ src/view/HomeView.vue
@@ -1,7 +1,6 @@
 <template>
   <div class=" relative w-full justify-start items-center overflow-hidden">
       <Swiper
-          align="center"
           :slidesPerView="1"
           :loop="true"
           :autoplay="
@@ -10,18 +9,18 @@
           }
          "
       >
-        <SwiperSlide >
+        <SwiperSlide>
           <div>
-            <img src="@/assets/img_home_2.jpg" alt="" class="w-full">
+            <img src="@/assets/img_home_2.jpg" alt="">
           </div>
         </SwiperSlide>
         <SwiperSlide >
           <div>
-          <img src="@/assets/banner1-scaled.jpg" alt="" class="w-full">
+          <img src="@/assets/banner1-scaled.jpg" alt="">
           </div>
         </SwiperSlide>
       </Swiper>
-    <div class="bg-[url('assets/1-1.png')] w-full flex justify-between max-lg:flex-col">
+    <div class="bg-[url('assets/1-1.png')] w-full flex justify-between max-lg:flex-col rounded-lg h-[600px]">
       <div class="pt-[150px]">
         <h1 class="text-[20px] text-[#1030bf] font-bold max-lg:text-[18px]">TIẾNG ANH NHƯ " GIÓ " CÙNG LANGSPACE</h1>
         <h1 class="text-[35px] text-[#1030bf] font-bold pt-[10px] max-lg:text-[30px]">
@@ -33,9 +32,9 @@
       <img src="@/assets/Lo-trinh-tach-nen-1.png" alt="" class="pt-[50px] w-[750px] max-lg:pt-[25px]">
     </div>
   </div>
-  <div class="bg-[url('assets/Background_web.jpg')] mt-5 h-[450px] w-full flex justify-between max-lg:flex-col bg-cover">
+  <div class="bg-[url('assets/Background_web.jpg')] mt-5 h-[450px] w-full flex justify-between max-lg:flex-col bg-cover rounded-lg h-[600px]">
       <div class="pt-[25px] pl-[100px]">
-        <h1 class="text-[60px] text-white font-bold">Chọn Langspace vì điều gì ?</h1>
+        <h1 class="text-[60px] text-white font-bold max-lg:text-[40px]">Chọn Langspace vì điều gì ?</h1>
         <div class="flex items-center py-2">
           <div class="h-5 w-5 rounded-full bg-[#ffbb01]"/>
           <p class="text-white text-lg font-bold ml-2">Thực chiến Tiếng Anh tối đa trên lớp và ngoại khoá</p>
@@ -57,8 +56,8 @@
           <p class="text-white text-lg font-bold ml-2">Giáo viên tận tâm, năng động, hài hước</p>
         </div>
         <button
-            class="bg-[#ffbb01] w-[200px] h-[50px] text-xl text-[#1030bf] font-bold flex items-center justify-center rounded-lg mt-5
-            hover:bg-[#1030bf] hover:text-[#ffbb01]
+            class="bg-[#ffbb01] w-[200px] h-[50px] text-xl text-[#1030bf] font-bold flex items-center justify-center rounded-lg mt-10
+            hover:bg-[#fff] hover:text-[#ffbb01]
 ">
           Đăng ký ngay
           <ArrowLongRightIcon class="size-6 ml-5 cursor-pointer"/>
